d8efbe0b5386b08ec305949c97346c0fbcb1e9da
lebo.mofokeng <lebo.mofokeng@DDNAD4R7DB22>
2022-06-30 12:34:20 +0200

    Add project files.

---
+++ fnb-account-Creation-LEBOHANG/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace fnb_account_Creation_LEBOHANG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBxFName = new System.Windows.Forms.TextBox();
+            this.txtBxEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rchTxtBxAddress = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCreateAccnt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::fnb_account_Creation_LEBOHANG.Properties.Resources.fnbFOOTER;
+            this.pictureBox2.Location = new System.Drawing.Point(128, 653);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(562, 86);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::fnb_account_Creation_LEBOHANG.Properties.Resources.First_National_Bank_Logo_svg;
+            this.pictureBox1.Location = new System.Drawing.Point(128, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(562, 179);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Full Name";
+            // 
+            // txtBxFName
+            // 
+            this.txtBxFName.Location = new System.Drawing.Point(372, 247);
+            this.txtBxFName.Name = "txtBxFName";
+            this.txtBxFName.Size = new System.Drawing.Size(226, 20);
+            this.txtBxFName.TabIndex = 3;
+            this.txtBxFName.TextChanged += new System.EventHandler(this.txtBxFName_TextChanged);
+            // 
+            // txtBxEmail
+            // 
+            this.txtBxEmail.Location = new System.Drawing.Point(373, 288);
+            this.txtBxEmail.Name = "txtBxEmail";
+            this.txtBxEmail.Size = new System.Drawing.Size(226, 20);
+            this.txtBxEmail.TabIndex = 5;
+            this.txtBxEmail.TextChanged += new System.EventHandler(this.txtBxEmail_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(202, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Email";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(373, 456);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(226, 20);
+            this.txtPhone.TabIndex = 7;
+            this.txtPhone.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(201, 463);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Phone ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(200, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Address";
+            // 
+            // rchTxtBxAddress
+            // 
+            this.rchTxtBxAddress.Location = new System.Drawing.Point(373, 331);
+            this.rchTxtBxAddress.Name = "rchTxtBxAddress";
+            this.rchTxtBxAddress.Size = new System.Drawing.Size(226, 96);
+            this.rchTxtBxAddress.TabIndex = 9;
+            this.rchTxtBxAddress.Text = "";
+            this.rchTxtBxAddress.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(202, 517);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 19);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Date of Birth";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // dateOfBirth
+            // 
+            this.dateOfBirth.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateOfBirth.Location = new System.Drawing.Point(372, 509);
+            this.dateOfBirth.Name = "dateOfBirth";
+            this.dateOfBirth.Size = new System.Drawing.Size(227, 22);
+            this.dateOfBirth.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Teal;
+            this.button1.Location = new System.Drawing.Point(544, 578);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 46);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCreateAccnt
+            // 
+            this.btnCreateAccnt.BackColor = System.Drawing.Color.Teal;
+            this.btnCreateAccnt.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateAccnt.ForeColor = System.Drawing.Color.Gold;
+            this.btnCreateAccnt.Location = new System.Drawing.Point(372, 578);
+            this.btnCreateAccnt.Name = "btnCreateAccnt";
+            this.btnCreateAccnt.Size = new System.Drawing.Size(150, 46);
+            this.btnCreateAccnt.TabIndex = 13;
+            this.btnCreateAccnt.Text = "Create Account";
+            this.btnCreateAccnt.UseVisualStyleBackColor = false;
+            this.btnCreateAccnt.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 767);
+            this.Controls.Add(this.btnCreateAccnt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateOfBirth);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rchTxtBxAddress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBxEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBxFName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBxFName;
+        private System.Windows.Forms.TextBox txtBxEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rchTxtBxAddress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateOfBirth;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCreateAccnt;
+    }
+}
+
---
+++ fnb-account-Creation-LEBOHANG/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace fnb_account_Creation_LEBOHANG
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=sqlserver.dynamicdna.co.za;Initial Catalog=fnb-account-creation-Lebohang;User ID=BBD;Password=123 ");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtBxFName.Clear();
+            txtBxEmail.Clear();
+            txtPhone.Clear();
+            rchTxtBxAddress.Clear();
+            dateOfBirth.Value = DateTime.Now;
+
+            txtBxFName.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (txtBxFName.Text.Length > 0 && txtBxEmail.Text.Length > 0 && rchTxtBxAddress.Text.Length > 0 && txtPhone.Text.Length > 0)
+            {
+
+
+                if (txtBxFName.Text.Length < 3)
+
+                    MessageBox.Show("please enter your full name");
+
+                if (txtBxEmail.Text.Contains(' '))
+
+                    MessageBox.Show("please enter valid email address");
+
+                if (txtPhone.TextLength > 12 || txtPhone.Text.Length < 10)
+
+                    MessageBox.Show("Pleae enter valid phone number");
+
+
+                if (2022 - dateOfBirth.Value.Year > 17)
+                {
+                    try
+                    {
+                        conn.Open();
+
+                        SqlCommand com = new SqlCommand("INSERT INTO Customer VALUES('"+txtBxFName.Text+"','"+txtBxEmail.Text+"','"+ txtPhone.Text+"','"+rchTxtBxAddress.Text+"', '"+dateOfBirth.Value+"') ", conn);
+
+                        try
+                        {
+                            com.ExecuteNonQuery();
+                            MessageBox.Show("Account created successfully!!");
+
+                            txtBxFName.Clear();
+                            txtBxEmail.Clear();
+                            txtPhone.Clear();
+                            rchTxtBxAddress.Clear();
+                            dateOfBirth.Value = DateTime.Now;
+
+                        }
+                        catch (Exception)
+                        {
+
+                            MessageBox.Show("Failed to create an account");
+                        }
+
+                        MessageBox.Show("Account created successfully!!");
+
+
+                        conn.Close();
+                    }
+                    catch (Exception)
+                    {
+
+                        MessageBox.Show("Failed to connect to Database");
+                    }
+
+                   
+                }
+                else
+
+                    MessageBox.Show("too young to open an account");
+            }
+            else
+            {
+               
+                MessageBox.Show("Please fill in al the required fields!!");
+            }
+
+        }
+
+        private void txtBxEmail_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtBxFName_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
